bbc105a778debe16f9c434d5f24ff8dde8eff067
Shysh Oleksandr <72020976+Shysh-Oleksandr@users.noreply.github.com>
2024-12-10 16:04:36 +0200

    Fix s3 image upload & delete (#36)

--- src/modules/notes/modules/s3.ts
+++ src/modules/notes/modules/s3.ts
@@ -13,7 +13,11 @@ const credentials: AwsCredentialIdentity = {
   secretAccessKey: AppConfig.awsSecretAccessKey ?? "",
 };
 
-const s3 = new S3Client({ credentials, region: "eu-north-1" });
+const s3 = new S3Client({
+  credentials,
+  region: "eu-north-1",
+  maxAttempts: 3,
+});
 
 type Props = {
   imagePath: string;
@@ -41,7 +45,7 @@ const uploadToS3 = async ({ imagePath, userId }: Props): ReturnType => {
   });
 
   try {
-    await s3.send(command);
+    s3.send(command);
 
     return { key };
   } catch (error) {
@@ -77,6 +81,9 @@ export const uploadLocalImagesToS3 = async (
 
   const uploadedImagesUri = await Promise.all(uploadPromises);
 
+  // this needed to avoid getting images from s3 right away as they're not available at that time and could return 403
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
   return uploadedImagesUri.filter((uri) => !!uri) as string[];
 };
 
@@ -91,7 +98,7 @@ const deleteFromS3 = async (imageUrl: string): ReturnType => {
   });
 
   try {
-    await s3.send(command);
+    s3.send(command);
 
     return { key };
   } catch (error) {
@@ -113,6 +120,8 @@ export const deleteImagesFromS3 = async (imagesUrl: string[]) => {
       return;
     }
   });
+
+  await new Promise((resolve) => setTimeout(resolve, 100));
 
   await Promise.all(uploadPromises);
 };
